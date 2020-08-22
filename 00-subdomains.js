8f9ea469297cb98d745e6964090e93c00989dfff
Chris Baker <chrisbaker@Chriss-MacBook-Pro.local>
2020-08-22 14:03:42 -0400

    vhost. trying to use admin.meadowlark domain

---
+++ 00-subdomains.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const vhost = require('vhost');
+const app = express();
+// creating an admin subdomain
+// this should appear before all other routes
+
+var admin = express.Router()
+app.use(vhost('admin.meadowlark.local', admin));
+
+// create admin routes; these can be defined anwywhere
+admin.get('*', (req, res) => { res.send('Welcome, Admin!') });
+
+// regular routes
+app.get('*', (req, res) => { res.send('Welcome, User!') });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Running subdomain.js on port ${port}`)
+});
